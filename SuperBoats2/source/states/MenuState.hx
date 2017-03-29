02af7b663c7025658627a1b72ee658b2b507d38c
0xFireball <0xFireball@outlook.com>
2017-03-29 11:16:35 -0700

    emitter menu effects

--- SuperBoats2/source/states/MenuState.hx
+++ SuperBoats2/source/states/MenuState.hx
@@ -18,18 +18,20 @@ class MenuState extends FlxState
 		titleTx.color = FlxColor.WHITE;
 		titleTx.screenCenter(FlxAxes.X);
 		titleTx.y = -titleTx.height;
-		FlxTween.tween(titleTx, { y: 200 }, 0.7, { ease: FlxEase.cubeOut });
-		add(titleTx);
+		var titleFinalY = 200;
+		FlxTween.tween(titleTx, { y: titleFinalY }, 0.7, { ease: FlxEase.cubeOut });
+		
 
-		emitter = new FlxEmitter(FlxG.width / 2, titleTx.y + titleTx.height / 2);
-		emitter.lifespan.set(0.6);
-		emitter.scale.set(1, 1, 4, 4, 6, 6, 6, 6);
+		emitter = new FlxEmitter(FlxG.width / 2, titleFinalY + titleTx.height * 1.2);
+		emitter.lifespan.set(0.4);
+		emitter.scale.set(2, 2, 8, 8, 12, 12, 12, 12);
 		emitter.makeParticles(1, 1, FlxColor.WHITE, 200);
 		// start emitter
-		emitter.start(false, 0.005);
-		emitter.velocity.set(-140, -140, 140, 140);
+		emitter.start(false, 0.003);
+		emitter.speed.set(120, 280);
 		emitter.color.set(FlxColor.fromRGBFloat(0.0, 0.4, 0.6), FlxColor.fromRGBFloat(0.4, 0.8, 1.0));
 		add(emitter);
+		add(titleTx); // add title after emitter
 
 		super.create();
 	}
