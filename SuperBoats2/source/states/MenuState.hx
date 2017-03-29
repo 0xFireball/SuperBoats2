7154a07dee355eaf30810916db36b0cd99877b5d
0xFireball <0xFireball@outlook.com>
2017-03-29 08:51:15 -0700

    add emitter

--- SuperBoats2/source/states/MenuState.hx
+++ SuperBoats2/source/states/MenuState.hx
@@ -3,20 +3,33 @@ package states;
 import flixel.*;
 import flixel.util.*;
 import flixel.tweens.*;
+import flixel.effects.particles.*;
 import ui.*;
 
 class MenuState extends FlxState
 {
 	private var titleTx:NFText;
+	
+	private var emitter:FlxEmitter;
 
 	override public function create():Void
 	{
-		titleTx = new NFText(0, FlxG.height * (1 / 4), "SuperBoats 2", 42);
+		titleTx = new NFText(0, 0, "SuperBoats 2", 84);
 		titleTx.color = FlxColor.WHITE;
 		titleTx.screenCenter(FlxAxes.X);
 		titleTx.y = -titleTx.height;
-		FlxTween.tween(titleTx, { y: FlxG.height * (1 / 4) }, 0.7, { ease: FlxEase.cubeOut })
+		FlxTween.tween(titleTx, { y: 200 }, 0.7, { ease: FlxEase.cubeOut });
 		add(titleTx);
+
+		emitter = new FlxEmitter(FlxG.width / 2, titleTx.y + titleTx.height / 2);
+		emitter.lifespan.set(0.6);
+		emitter.scale.set(1, 1, 4, 4, 6, 6, 6, 6);
+		emitter.makeParticles(1, 1, FlxColor.WHITE, 200);
+		// start emitter
+		emitter.start(false, 0.005);
+		emitter.velocity.set(-140, -140, 140, 140);
+		emitter.color.set(FlxColor.fromRGBFloat(0.0, 0.4, 0.6), FlxColor.fromRGBFloat(0.4, 0.8, 1.0));
+		add(emitter);
 
 		super.create();
 	}
