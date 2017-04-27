629e20f005fd1d8e032586fcca08c72f56e9d13e
0xFireball <0xFireball@outlook.com>
2017-04-26 22:43:59 -0700

    fix thresholds

--- SuperBoats2/source/sprites/ui/HUD.hx
+++ SuperBoats2/source/sprites/ui/HUD.hx
@@ -46,20 +46,20 @@ class HUD extends FlxSpriteGroup {
 
         if (stateData.player.shieldPercentage > 92) {
             shieldIntText.color = FlxColor.fromRGBFloat(0.7, 1.0, 0.7);
-        } else if (stateData.player.shieldPercentage < 60) {
-            shieldIntText.color = FlxColor.fromRGBFloat(1.0, 1.0, 0.7);
         } else if (stateData.player.shieldPercentage < 25) {
             shieldIntText.color = FlxColor.fromRGBFloat(1.0, 0.7, 0.7);
+        } else if (stateData.player.shieldPercentage < 60) {
+            shieldIntText.color = FlxColor.fromRGBFloat(1.0, 1.0, 0.7);
         } else {
             shieldIntText.color = FlxColor.WHITE;
         }
 
         if (stateData.player.healthPercentage > 92) {
             hullIntText.color = FlxColor.fromRGBFloat(0.7, 1.0, 0.7);
-        } else if (stateData.player.healthPercentage < 60) {
-            hullIntText.color = FlxColor.fromRGBFloat(1.0, 1.0, 0.7);
         } else if (stateData.player.healthPercentage < 25) {
             hullIntText.color = FlxColor.fromRGBFloat(1.0, 0.7, 0.7);
+        } else if (stateData.player.healthPercentage < 60) {
+            hullIntText.color = FlxColor.fromRGBFloat(1.0, 1.0, 0.7);
         } else {
             shieldIntText.color = FlxColor.WHITE;
         }
