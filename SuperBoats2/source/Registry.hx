37868df2299ea4f7b4b9a495ccbe752193da99dc
0xFireball <0xFireball@outlook.com>
2017-04-27 09:33:33 -0700

    mg update

--- SuperBoats2/source/Registry.hx
+++ SuperBoats2/source/Registry.hx
@@ -4,7 +4,7 @@ package;
 class Registry {
     // constants
 
-    public static var gameVersion:String = "v0.1.2.4 alpha";
+    public static var gameVersion:String = "v0.1.3.0 alpha";
 
     // vars
 
