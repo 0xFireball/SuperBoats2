fbb48a7686762bb4dd9d75389a53f7bbc65340d7
0xFireball <0xFireball@outlook.com>
2017-04-27 23:57:08 -0700

    bugfix version bump

--- SuperBoats2/source/Registry.hx
+++ SuperBoats2/source/Registry.hx
@@ -6,7 +6,7 @@ import flixel.util.FlxSave;
 class Registry {
     // constants
 
-    public static var gameVersion:String = "v0.1.3.2 alpha";
+    public static var gameVersion:String = "v0.1.3.3 alpha";
 
     // vars
 
