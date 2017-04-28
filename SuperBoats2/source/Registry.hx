1e3415510214c4c3149e7291451ca98ad19e7d08
0xFireball <0xFireball@outlook.com>
2017-04-28 11:09:31 -0700

    update stuff

--- SuperBoats2/source/Registry.hx
+++ SuperBoats2/source/Registry.hx
@@ -6,7 +6,7 @@ import flixel.util.FlxSave;
 class Registry {
     // constants
 
-    public static var gameVersion:String = "v0.1.3.4 alpha";
+    public static var gameVersion:String = "v0.1.4.0 alpha";
 
     // vars
 
